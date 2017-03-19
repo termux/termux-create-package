cae4ddeccd40543f010efbf76fb873c93abff97c
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-19 02:57:57 +0100

    Include README in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
 # https://packaging.python.org/distributing/#packaging-your-project
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 setup(
     name = 'termux-create-package',
     version = '0.4',
     license = 'MIT',
     description = 'Lightweight tool for creating deb packages',
+    long_description = readme(),
     author = 'Fredrik Fornwall',
     author_email = 'fredrik@fornwall.net',
     url = 'https://github.com/termux/termux-create-package',
